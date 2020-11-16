8976460596abc59ba974139fbb26a192326cd3fa
BNivia <nobenjamin19@gmail.com>
2020-11-16 16:33:21 -0500

    Added Java file for mileage.

---
+++ Milage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Mileage{
+    /**Testing 1,2,3*/
+    public static void main (String[] args){
+        //Sttributes
+        int miles;
+        double gallons, mpg;
+        //mpg - miles/gallons;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter miles and gallons: ");
+        miles - s.nextInt();
+        gallons - s.nextDouble();
+        pmg = miles/gallons;
+
+        //output
+        System.out.println("Miles" + "Per" + "Gallon" + mpg);
+    }
+}
